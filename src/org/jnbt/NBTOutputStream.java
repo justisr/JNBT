faae7a4d432fb8a6705fd0eced09f28b1d78e593
Justis R <justis.root@gmail.com>
2022-07-02 03:34:26 +0100

    Update NBTOutputStream.java

--- src/org/jnbt/NBTOutputStream.java
+++ src/org/jnbt/NBTOutputStream.java
@@ -255,7 +255,7 @@ public class NBTOutputStream implements Closeable {
 	 * @throws IOException
 	 *             if an I/O error occurs.
 	 */
-	void writeStringTagPayload(final StringTag tag) throws IOException {
+	protected void writeStringTagPayload(final StringTag tag) throws IOException {
 	
 		final byte[] bytes = tag.getValue().getBytes(NBTConstants.CHARSET);
 		os.writeShort(bytes.length);
